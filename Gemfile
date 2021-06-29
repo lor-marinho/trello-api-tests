c7e338783ff6efa7e897c768d58c311bc023d4f4
Lorena Marinho <lor.marinho@gmail.com>
2021-06-29 16:59:33 -0300

    halfway through instructions

--- Gemfile
+++ Gemfile
@@ -24,6 +24,13 @@ gem 'jbuilder', '~> 2.7'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
+gem 'httparty'
+gem "addressable"
+gem "awesome_print"
+gem "http"
+gem 'pry-byebug'
+
+gem 'dotenv-rails', groups: [:development, :test]
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,72 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'httparty'
+require 'awesome_print'
+require "addressable/uri"
+require "http"
+require 'pry-byebug'
+
+def app_key
+    ENV.fetch('TRELLO_APP_KEY')
+end
+
+def app_token
+    ENV.fetch('TRELLO_APP_TOKEN')
+end
+
+def trello_url(path, params = {})
+  auth_params = { key: app_key, token: app_token }
+
+  Addressable::URI.new({
+    scheme: "https",
+    host: "api.trello.com",
+    path: File.join("1", path),
+    query_values: auth_params.merge(params)
+  })
+end
+
+
+
+
+# url = "https://api.trello.com/1/members/me?key=#{api_key}&token=#{api_token}"
+
+    # response = HTTParty.get(trello_url(path))
+    # response_body = JSON.parse(response.body)
+
+def get(path)
+    HTTP.get(trello_url(path)).parse
+end
+
+MAX = 1000
+
+def paginated_get(path, options = {})
+  Enumerator.new do |y|
+    params  = options.dup
+    before  = nil
+    total   = 0
+    limit   = params.delete(:limit) { 50 }
+
+    loop do
+      data = get(path, { before: before, limit: limit }.merge(params))
+      total += data.length
+
+      data.each do |element|
+        y.yield element
+      end
+
+      break if (data.empty? || total >= MAX)
+
+      before = data.last["id"]
+    end
+  end
+end
+
+puts paginated_get("members/me/actions", filter: "commentCard")
+
+
+
+# ap response_body
+
+# puts get
